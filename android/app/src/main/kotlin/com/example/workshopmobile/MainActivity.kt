50add35d39c17255329280aa851767a08ab70540
bgsola <bagoesolea@gmail.com>
2022-09-17 23:08:22 +0700

    create workshop mobile

---
+++ android/app/src/main/kotlin/com/example/workshopmobile/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.workshopmobile
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,76 @@
+import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          backgroundColor: Colors.white,
+          title: Row(
+              // ignore: prefer_const_literals_to_create_immutables
+              children: [
+                const Text(
+                  "Sri Wulandari",
+                  style: TextStyle(color: Colors.black),
+                ),
+                Icon(
+                  Icons.alarm,
+                  color: Colors.red[500],
+                )
+                // Image(image: new AssetImage("assets/icon/bell.png"), width: 150)
+                // Expanded(
+                //   child: Image.asset('images/pic1.jpg'),
+                // )
+              ],
+          )
+        ),
+        body: const Center(
+          child: RandomWords(),
+        ),
+      )
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  const RandomWords({Key? key}) : super(key: key);
+
+  @override
+  State<RandomWords> createState() => _RandomWordsState();
+}
+
+class _RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _biggerFont = const TextStyle(fontSize: 18);
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: /*1*/ (context, i) {
+        if (i.isOdd) return const Divider(); /*2*/
+
+        final index = i ~/ 2; /*3*/
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+        }
+        return ListTile(
+          title: Text(
+            _suggestions[index].asPascalCase,
+            style: _biggerFont,
+          ),
+        );
+      },
+    );
+  }
+}
+
